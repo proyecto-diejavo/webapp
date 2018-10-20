dcc24269ecdef6c3ae35b2f8e8d5258432d9bcdf
Diego Rodriguez <diegoz1028@gmail.com>
2018-10-19 19:41:01 -0500

    Updates

---
+++ precache-manifest.86673faace3f17345000247166444051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "65c26c62aa662cc6acef",
+    "url": "/webapp/static/js/runtime~main.65c26c62.js"
+  },
+  {
+    "revision": "143bd78b9c8b9580cae5",
+    "url": "/webapp/static/js/main.143bd78b.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/webapp/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "143bd78b9c8b9580cae5",
+    "url": "/webapp/static/css/main.09366621.chunk.css"
+  },
+  {
+    "revision": "48ace63c9c6ff506d6b3a02df765e590",
+    "url": "/webapp/index.html"
+  }
+];
